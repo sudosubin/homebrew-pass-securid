6a1bbab38c15399a5ca14b31d29db7461bbd6946
sudosubin <sudosubin@gmail.com>
2022-03-01 16:32:37 +0900

    release: 0.1.1

--- Formula/pass-securid.rb
+++ Formula/pass-securid.rb
@@ -1,9 +1,11 @@
 class PassSecurid < Formula
   desc "A pass extension for managing RSA SecurIDs"
   homepage "https://github.com/sudosubin/pass-securid#readme"
-  url "https://github.com/sudosubin/pass-securid/archive/99e5da3fa7e2b5c613a3c275de182b82facd54a5.tar.gz"
-  sha256 "ead1e485860825f2d6d194b7a1e2f37a3d40b901ffd30e06e8e540ec1a745a74"
+  version "0.1.1"
   license "GPL-3.0"
+
+  url "https://github.com/sudosubin/pass-securid/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "d1e43f628edecb1d7d1032fee7a1814cfd6175123ee1d9be0f675ec01316727a"
 
   depends_on "pass"
   depends_on "stoken"
